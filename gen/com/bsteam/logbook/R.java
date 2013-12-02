dce0c63bdfc5ec906f02af3ec1869fdd037811d6
krishnagopinath <hpkrishna07@gmail.com>
2013-12-03 03:22:32 +0530

    .

--- gen/com/bsteam/logbook/R.java
+++ gen/com/bsteam/logbook/R.java
@@ -26,13 +26,11 @@ public final class R {
     public static final class id {
         public static final int DialogList=0x7f080001;
         public static final int RecordSndBtn=0x7f080003;
-        public static final int TaskSaveBtn=0x7f080002;
-        public static final int TitleTextView=0x7f080005;
-        public static final int action_settings=0x7f080008;
-        public static final int listView1=0x7f080007;
+        public static final int TaskSaveBtn=0x7f080004;
+        public static final int action_settings=0x7f080006;
+        public static final int listView1=0x7f080005;
         public static final int pager=0x7f080000;
-        public static final int saveTextBox=0x7f080004;
-        public static final int textView1=0x7f080006;
+        public static final int saveTextBox=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
